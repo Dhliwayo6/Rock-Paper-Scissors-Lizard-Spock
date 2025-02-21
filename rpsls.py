349743f27605dbd7302fe7ddd7ecafa16538d412
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-02-21 19:15:51 +0200

    outcome generator function complete

--- rpsls.py
+++ rpsls.py
@@ -2,7 +2,20 @@ import random
 
 
 def main():
-    ...
+    print(generate_outcomes())
+
+def generate_outcomes():
+
+    outcomes = {}
+
+    outcomes["scissors"] = ("paper", "lizard")
+    outcomes["paper"] = ("rock", "spock")
+    outcomes["rock"] = ("lizard")
+    outcomes["lizard"] = ("spock", "paper")
+    outcomes["spock"] = ("scissors", "rock") 
+
+    return outcomes
+
 
 if __name__ == "__main__":
     main()
